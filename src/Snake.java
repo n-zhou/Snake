e4ecedfeaecff97a0b75a458f722e963c165ba17
nzho8446 <nzho8446@uni.sydney.edu.au>
2018-06-10 00:24:52 +1000

    xxx

---
+++ src/Snake.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
+public class Snake {
+
+    public static void main(String... args) {
+
+    }
+
+}
