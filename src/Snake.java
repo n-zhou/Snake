50c5cfd585fc775d2e2d417e59a9a2230eca4f74
nzho8446 <nzho8446@uni.sydney.edu.au>
2018-06-10 00:37:41 +1000

    changed things

--- src/Snake.java
+++ src/Snake.java
@@ -7,7 +7,12 @@ import java.awt.Point;
 
 public class Snake {
 
-    public static void main(String... args) {
+    public static void main(String[] args) {
+        System.out.printf("Hello\n");
+        JFrame frame = new JFrame("Snake");
+        frame.setSize(600,300);
+        frame.show();
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
     }
 
